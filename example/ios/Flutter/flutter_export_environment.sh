4c5738fdabc39e5ba3e6c79669b198de621fe60e
Ahmed Mozaly <ahmed.mozaly@freedomdev.com>
2019-11-07 11:52:33 +0200

    clean up shell file

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Program Files\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Mozaly\MoveIT\new_geolocation\example"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/Alfanhui/Library/Android/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/Alfanhui/Nextcloud/Code/dart/new_geolocation/example"
+export "FLUTTER_TARGET=/Users/Alfanhui/Nextcloud/Code/dart/new_geolocation/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\Program Files\flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/Alfanhui/Library/Android/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
